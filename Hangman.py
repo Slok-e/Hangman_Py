99bf03da17f100536a4f4e3ecbd55ef96fc90b3e
Slok-e <106457601+Slok-e@users.noreply.github.com>
2022-08-01 12:11:09 -0400

    Update
    Fixed input list and renamed list file name to Animals.py
    Begin to right code for hangman as well as input a image format to make game more interactive.

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,134 @@
+import logging
 import random
-import math
+from Animals import Animals
 
-def Pick Category()
+def get_words():
+    word = random.choice(Animals)
+    return word.upper
+
+def play(word):
+    name = input("Welcome, what is your name: ")
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print(f"Welcome, {name}.\n Lets play a game of Hangman")
+    print(display_hangman(tries))
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or Word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print(f"You already guessed this letter, {guess}")
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print(f" Good job, {guess}, is in the word.")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+        
+        else:
+            print("Not a Valid Guess")
+            print(display_hangman(tries))
+            print(word_completion)
+            print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
